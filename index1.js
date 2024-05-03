b53348c2057b98341e01bce771ade0ea3b8bbc9c
ashwani7985 <ashwanipaswan.engg@gmail.com>
2024-05-03 11:30:55 +0530

    install package

--- index1.js
+++ index1.js
@@ -2,10 +2,25 @@ require('dotenv').config();
 const bodyParser = require('body-parser');
 const express = require('express');
 const { default: mongoose }= require('mongoose');
+const cors = require('cors')
 const app = express();
+const port = process.env.PORT || 3000
 const conn = process.env.CONN;
 
 app.use(bodyParser.json());
+
+const allowCrossDomain = (req, res, next)=>{
+    res.header(`Access-Control-Allow-Origin`, `*`);
+    res.header(`Access-Control-Allow-Methods`, `GET,PUT,POST,DELETE`);
+    res.header(`Access-Control-Allow-Header`, `Content-Type`);
+    next();
+};
+
+app.use(cors())
+app.use(allowCrossDomain)
+app.options('*',cors())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
 
 mongoose.connect(conn)
 .then(()=>console.log('connected'))
@@ -45,6 +60,19 @@ const demoSchema = new mongoose.Schema({
 })
 
 const user = mongoose.model('mymodel',demoSchema,'demodata')
+
+app.get("/", (req, res) => {
+    res.end("Hello world")
+})
+
+app.get("/about", (req, res) => {
+    res.end("welcome to about page")
+})
+
+app.get("/name/:myname", (req, res) => {
+    res.end("welcome " + req.params.myname)
+})
+
 app.get('/count', async (req, res) =>{
     res.json({count: await user.countDocuments()})
 })
@@ -69,4 +97,4 @@ app.get('/id/:name', async(req,res)=>{
     res.json({msg : "success", data : namedUser})
 }) 
 
-app.listen(8081, ()=> console.log("Appliction started"))
+app.listen(port, ()=> console.log("Appliction started "+port));
