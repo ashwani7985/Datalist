983dbd04fd1f2cba12fec0621ad270d1e41e1cf7
ashwani7985 <ashwanipaswan.engg@gmail.com>
2024-05-06 14:36:55 +0530

    file change

--- db/user.js
+++ db/user.js
@@ -3,7 +3,7 @@ const todoSchema = require("./todoSchema");
 const userSchema = require("./userSchema");
 
 const DB_NAME = process.env.DB_NAME || "todoApp";
-const URI = process.env.MONGO_URI; 
+const URI = process.env.MONGO_URI || "mongodb+srv://temp:temp@cluster0.632lzyb.mongodb.net"; 
 const MONGO_URI = `${URI}/${DB_NAME}`;
 
 mongoose.connect(MONGO_URI).then(()=>console.log("Connected")).catch((err)=>console.log(err));
