99abfc1d0426cbd268974fe5ebdad2033011f625
ashwani7985 <ashwanipaswan.engg@gmail.com>
2024-05-10 16:40:21 +0530

    file change

--- app/src/main/java/com/example/vehicleinsurance/LoginActivity.java
+++ app/src/main/java/com/example/vehicleinsurance/LoginActivity.java
@@ -2,77 +2,146 @@ package com.example.vehicleinsurance;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
-import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
+import org.json.JSONException;
+import org.json.JSONObject;
 
-public class LoginActivity extends AppCompatActivity{
+import java.util.HashMap;
+import java.util.Map;
 
-   private Button btn;
-   private TextView email, pass;
+public class LoginActivity extends AppCompatActivity {
+
+    private Button btn;
+    private TextView email, pass;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        btn = findViewById(R.id.button);
+        btn = findViewById(R.id.btn1);
         email = findViewById(R.id.email_input);
         pass = findViewById(R.id.password_input);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                final String emailBody = email.getText().toString();
-                final String passBody = pass.getText().toString();
-                sendDataToAPI(emailBody,passBody);
+                loginUser();
             }
         });
 
     }
-    private void sendDataToAPI(String email, String password){
-        try {
-        String apiEndpoint="https://web-dev-1-v7lo.onrender.com/login";
-            final String data = URLEncoder.encode("email","UTF-8")+"="+URLEncoder.encode(email,"UTF-8")+"&"+URLEncoder.encode("password","UTF-8")+"="+URLEncoder.encode(password,"UTF-8");
 
-            RequestQueue requestQueue = Volley.newRequestQueue(this);
+    private void loginUser() {
+        String emailBody = email.getText().toString();
+        String passBody = pass.getText().toString();
+        String apiEndpoint = "https://web-dev-1-v7lo.onrender.com/login";
 
-            StringRequest stringRequest = new StringRequest(Request.Method.POST, apiEndpoint, new Response.Listener<String>() {
-                @Override
-                public void onResponse(String response) {
-                    Toast.makeText(LoginActivity.this, "Data stored successfully!", Toast.LENGTH_SHORT).show();
-                }
-            }, new Response.ErrorListener() {
-                @Override
-                public void onErrorResponse(VolleyError error) {
-                    Toast.makeText(LoginActivity.this, "Error storing data "+error.getMessage(),Toast.LENGTH_SHORT).show();
-                }
-            }){
-                public byte[] getBody() throws AuthFailureError{
-                    try{
-                        return data.getBytes("utf-8");
-                    }catch (UnsupportedEncodingException e){
-                        e.printStackTrace();
-                        return null;
-                    }
-                }
-            };
-            requestQueue.add(stringRequest);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        StringRequest sreq = new StringRequest(Request.Method.POST, apiEndpoint,
+                res -> Toast.makeText(getApplicationContext(), "Response: " + res, Toast.LENGTH_LONG).show(),
+                err -> Toast.makeText(getApplicationContext(), "Error: " + err, Toast.LENGTH_LONG).show())
+        {
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<>();
+
+                params.put("username", emailBody);
+                params.put("pass", passBody);
+
+                return params;
+            }
+        };
+        requestQueue.add(sreq);
     }
-
 }
+
+
+
+
+
+
+
+//    private void loginUser() {
+//        String emailBody = email.getText().toString();
+//        String passBody = pass.getText().toString();
+//        String apiEndpoint = "https://web-dev-1-v7lo.onrender.com/login";
+//        RequestQueue requestQueue = Volley.newRequestQueue(this);
+
+//        JSONObject jsonObject = new JSONObject();
+//        try {
+//            jsonObject.put("email", emailBody);
+//            jsonObject.put("password", passBody);
+//        } catch (JSONException e) {
+//            e.printStackTrace();
+//        }
+//        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, apiEndpoint, jsonObject, new Response.Listener<JSONObject>() {
+//            @Override
+//            public void onResponse(JSONObject response) {
+//                try {
+//                    boolean success = response.getBoolean("success");
+//                    if (success) {
+//                        Toast.makeText(LoginActivity.this, "login successfully!", Toast.LENGTH_SHORT).show();
+//                    } else {
+//                        Toast.makeText(LoginActivity.this, "login failed ", Toast.LENGTH_SHORT).show();
+//                    }
+//                } catch (JSONException e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//        }, new Response.ErrorListener() {
+//            @Override
+//            public void onErrorResponse(VolleyError error) {
+//                Log.e("LoginActivity", "Error: " + error.getMessage());
+//                Toast.makeText(LoginActivity.this, "Error occurred " + error.getMessage(), Toast.LENGTH_SHORT).show();
+//            }
+//        });
+
+
+
+//    private void sendDataToAPI(String email, String password){
+//        try {
+//        String apiEndpoint="https://web-dev-1-v7lo.onrender.com/login";
+//            final String data = URLEncoder.encode("email","UTF-8")+"="+URLEncoder.encode(email,"UTF-8")+"&"+URLEncoder.encode("password","UTF-8")+"="+URLEncoder.encode(password,"UTF-8");
+//
+//            RequestQueue requestQueue = Volley.newRequestQueue(this);
+//
+//            StringRequest stringRequest = new StringRequest(Request.Method.POST, apiEndpoint, new Response.Listener<String>() {
+//                @Override
+//                public void onResponse(String response) {
+//                    Toast.makeText(LoginActivity.this, "Data stored successfully!", Toast.LENGTH_SHORT).show();
+//                }
+//            }, new Response.ErrorListener() {
+//                @Override
+//                public void onErrorResponse(VolleyError error) {
+//                    Toast.makeText(LoginActivity.this, "Error storing data "+error.getMessage(),Toast.LENGTH_SHORT).show();
+//                }
+//            }){
+//                public byte[] getBody() throws AuthFailureError{
+//                    try{
+//                        return data.getBytes("utf-8");
+//                    }catch (UnsupportedEncodingException e){
+//                        e.printStackTrace();
+//                        return null;
+//                    }
+//                }
+//            };
+//            requestQueue.add(stringRequest);
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//  }
+
+//}
